e71036e783fed0626cbecc941b639a32c95649e5
alaa <aamal79545@gmail.com>
2024-03-01 19:37:54 +0200

    Add project files.

---
+++ Valid-Parentheses/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Parentheses
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string testString = "{[()]}";
+            bool isValid = IsValidParentheses(testString);
+            Console.WriteLine($"Is Valid: {isValid}");
+            Console.ReadKey();
+        }
+
+        static bool IsValidParentheses(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')' && (stack.Count == 0 || stack.Pop() != '('))
+                {
+                    return false;
+                }
+                else if (c == '}' && (stack.Count == 0 || stack.Pop() != '{'))
+                {
+                    return false;
+                }
+                else if (c == ']' && (stack.Count == 0 || stack.Pop() != '['))
+                {
+                    return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
+
